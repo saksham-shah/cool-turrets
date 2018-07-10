e4142e30bd9b18f2f2948546bf55154bacb78fb1
Saksham Shah <40961628+saksham-shah@users.noreply.github.com>
2018-07-10 21:15:20 +0100

    Add friction parameter to constructor

--- entity.js
+++ entity.js
@@ -1,11 +1,12 @@
 // Base object for game objects that have a pos, vel and acc (all vectors)
-function Entity(x_, y_, r_) {
+function Entity(x_, y_, friction_, r_) {
     this.pos = createVector(x_, y_);
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
 
     this.maxVel = 3;
     this.maxForce = 0.1;
+    this.friction = friction_;
 
     // R for radius
     this.r = r_;
@@ -13,11 +14,11 @@ function Entity(x_, y_, r_) {
 
 // Moves using pos, vel and acc
 Entity.prototype.move = function() {
-    this.pos.add(this.vel.mult(dt));
+    this.pos.add(p5.Vector.mult(this.vel, dt));
     this.vel.add(this.acc);
     this.vel.limit(this.maxVel);
     this.acc.mult(0);
 
     //Apply friction
-    this.vel.mult(0.96);
+    this.vel.mult(this.friction);
 };
