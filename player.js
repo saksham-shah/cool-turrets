d94d03115a6172c2fd1075e77133c8d2869863c7
Saksham Shah <40961628+saksham-shah@users.noreply.github.com>
2018-07-10 16:17:38 +0100

    Update player.js

--- player.js
+++ player.js
@@ -6,3 +6,36 @@ function Player(x_, y_) {
 
 // Adds the Entity prototype to the Player object
 Player.prototype = Object.create(Entity.prototype);
+
+Player.prototype.update = function() {
+  // this.seekMouse();
+  this.followMouse();
+}
+
+// Follows mouse by accelerating towards it
+Player.prototype.followMouse = function() {
+  var mousePos = createVector(mouseX, mouseY);
+  var vectorToMouse = p5.Vector.sub(mousePos, this.pos);
+  vectorToMouse.normalize();
+  vectorToMouse.mult(this.maxVel);
+  this.acc.add(vectorToMouse);
+}
+
+// Follows mouse using desired velocity stuff
+Player.prototype.seekMouse = function() {
+  var mousePos = createVector(mouseX, mouseY);
+  var vectorToMouse = p5.Vector.sub(mousePos, this.pos);
+  vectorToMouse.normalize();
+  vectorToMouse.mult(this.maxVel);
+  vectorToMouse.sub(this.vel);
+  vectorToMouse.limit(this.maxForce);
+  this.acc.add(vectorToMouse);
+}
+
+
+// Just a white circle right now
+Player.prototype.draw = function() {
+  fill(255);
+  noStroke();
+  ellipse(this.pos.x, this.pos.y, this.r * 2);
+}
