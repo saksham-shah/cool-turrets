fecdf8c4d7c85d08624042f54536a87a1204f07f
Saksham Shah <40961628+saksham-shah@users.noreply.github.com>
2018-07-10 16:18:10 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+var player;
+
 function setup() {
   createCanvas(600, 400);
+  player = new Player(0, 0);
 }
 
 function draw() {
   background(50);
+  player.update();
+  player.move();
+  player.draw();
+
 }
