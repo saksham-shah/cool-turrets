368592ff2937519f7bd255cd8511878acf9971bd
Saksham Shah <40961628+saksham-shah@users.noreply.github.com>
2018-07-10 20:11:57 +0100

    Turret inherits from Entity

--- turret.js
+++ turret.js
@@ -1,9 +1,14 @@
 function Turret(x_, y_) {
-    this.r = 5;
-    this.pos = createVector(x_, y_);
-    this.direction = createVector(0, 1);
+    // Inherit from Entity
+    Entity.call(this, x_, y_, 5);
+    // this.r = 5;
+    // this.pos = createVector(x_, y_);
+    this.direction = createVector(0, 1); // It might make sense for this to be a number in radians
     this.playerControlRadius = 200;
 }
+
+// Adds the Entity prototype to the Turret object
+Player.prototype = Object.create(Entity.prototype);
 
 Turret.prototype.update = function() {
     if (this.pos.dist(player.pos) < this.playerControlRadius) {
@@ -16,6 +21,7 @@ Turret.prototype.draw = function() {
     push();
     translate(this.pos.x, this.pos.y);
     rotate(this.direction.heading());
+    
     //Draw control radius
     fill(0, 255, 0, 10);
     ellipse(0, 0, this.playerControlRadius * 2);
