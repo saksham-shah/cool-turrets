4e337348658e41f18a8f8089dacb294dd10a29c8
Saksham Shah <40961628+saksham-shah@users.noreply.github.com>
2018-07-10 20:34:12 +0100

    Update turret construction with colour argument

--- sketch.js
+++ sketch.js
@@ -8,13 +8,13 @@ function setup() {
     player = new Player(0, 0);
     turrets = [];
 
-    turrets.push(new Turret(width / 2, height / 2));
+    turrets.push(new Turret(width / 2, height / 2, color(255, 0, 0)));
 
     lastUpdate = Date.now();
 }
 
 function draw() {
-    //Calculate Delta time in order to have smooth movement
+    // Calculate Delta time in order to have smooth movement
     var now = Date.now();
     dt = (now - lastUpdate) / (1000 / 60); //dt will be 1 at 60fps
     lastUpdate = now;
