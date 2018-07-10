9d92beb9381f633be6a1e56a94057cb19a040709
Saksham Shah <40961628+saksham-shah@users.noreply.github.com>
2018-07-10 16:17:54 +0100

    Update entity.js

--- entity.js
+++ entity.js
@@ -4,6 +4,9 @@ function Entity(x_, y_, r_) {
   this.vel = createVector(0, 0);
   this.acc = createVector(0, 0);
 
+  this.maxVel = 3;
+  this.maxForce = 0.1;
+
   // R for radius
   this.r = r_;
 }
@@ -12,5 +15,6 @@ function Entity(x_, y_, r_) {
 Entity.prototype.move = function() {
   this.pos.add(this.vel);
   this.vel.add(this.acc);
+  this.vel.limit(this.maxVel);
   this.acc.mult(0);
 }
