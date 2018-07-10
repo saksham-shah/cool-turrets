ac509d7405608d43e03c6e0ec21d1bb34052a46b
Saksham Shah <40961628+saksham-shah@users.noreply.github.com>
2018-07-10 20:56:59 +0100

    Change direction to a number in radians
    Change from Vector object to number so it is easier to work with

--- turret.js
+++ turret.js
@@ -3,7 +3,7 @@ function Turret(x_, y_, colour_) {
     Entity.call(this, x_, y_, 20);
     // this.r = 5;
     // this.pos = createVector(x_, y_);
-    this.direction = createVector(0, 1); // It might make sense for this to be a number in radians
+    this.direction = 0;
     this.playerControlRadius = 200;
     this.colour = colour_;
 }
@@ -13,15 +13,18 @@ Turret.prototype = Object.create(Entity.prototype);
 
 Turret.prototype.update = function() {
     if (this.pos.dist(player.pos) < this.playerControlRadius) {
-        //Player controlled
-        this.direction = player.pos.copy().sub(this.pos).normalize();
+        // Player controlled
+        var vectorPlayerToTurret = p5.Vector.sub(this.pos, player.pos);
+
+        this.direction = vectorPlayerToTurret.heading();
+        // this.direction = player.pos.copy().sub(this.pos).normalize();
     }
 }
 
 Turret.prototype.draw = function() {
     push();
     translate(this.pos.x, this.pos.y);
-    rotate(this.direction.heading());
+    rotate(this.direction);
     
     // Draw control radius
     fill(0, 255, 0, 10);
@@ -36,7 +39,7 @@ Turret.prototype.draw = function() {
     noStroke();
     ellipse(0, 0, this.r * 2);
     // Draw gun
-    rect(-30, -3, 30, 6);    
+    rect(0, -3, 30, 6);    
     
     pop();
 }
