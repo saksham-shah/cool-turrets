1576e45970b5b37646f88c6754c9ec67e00d9565
judah-daniels <judahdaniels@outlook.com>
2018-07-11 01:20:26 +0100

    Added movement and shooting
    Added movement(testing)
    And basic shooting mechanics

--- turret.js
+++ turret.js
@@ -1,46 +1,75 @@
 function Turret(x_, y_, colour_) {
     // Inherit from Entity
     Entity.call(this, x_, y_, 1, 20); // 1 for friction means there is no friction - turrets glide forever
-    // this.r = 5;
-    // this.pos = createVector(x_, y_);
+
     this.direction = 0;
     this.playerControlRadius = 50;
     this.colour = colour_;
-    // this.vel.y = 0.1;
+    this.vel.y = 1;
+    this.reloadTime = 60;
+    this.reloadTimer = this.reloadTime;
 }
 
 // Adds the Entity prototype to the Turret object
 Turret.prototype = Object.create(Entity.prototype);
 
 Turret.prototype.update = function() {
+    //Shoot Timer
+    this.reloadTimer -= dt;
+
+    //Check if player within range of turret
     if (this.pos.dist(player.pos) < this.playerControlRadius + player.r) {
         // Player controlled
         var vectorPlayerToTurret = p5.Vector.sub(this.pos, player.pos);
 
         this.direction = vectorPlayerToTurret.heading();
-        // this.direction = player.pos.copy().sub(this.pos).normalize();
+
+
+        if (this.reloadTimer < 0) {
+            this.reloadTimer = this.reloadTime;
+            this.shoot();
+        }
     }
-}
+
+
+
+    //When off the screen, reposition turret
+    if (this.pos.x - this.r > width) {
+        this.pos.x = -this.r;
+        this.pos.y = random(this.r, height - this.r);
+    } else if (this.pos.y - this.r > height) {
+        this.pos.y = -this.r;
+        this.pos.x = random(this.r, width - this.r);
+    } else if (this.pos.x + this.r < 0) {
+        this.pos.y = random(this.r, height - this.r);
+        this.pos.x = width + this.r;
+    } else if (this.pos.y + this.r < 0) {
+        this.pos.y = height + this.r;
+        this.pos.x = random(this.r, width - this.r);
+
+    }
+};
+
+Turret.prototype.shoot = function() {
+    entities.push(new Bullet(this.pos.x, this.pos.y, this.direction, 10, this.colour));
+};
 
 Turret.prototype.draw = function() {
     push();
     translate(this.pos.x, this.pos.y);
     rotate(this.direction);
-    
+
     // Draw control radius
     fill(0, 255, 0, 10);
     ellipse(0, 0, this.playerControlRadius * 2);
 
-    // Draw Turret
-    //fill(255, 0, 0);
-    //rectMode(CENTER);
-    //rect(0, 0, 30, 50);
-    
+    // Draw turret body
     fill(this.colour);
     noStroke();
     ellipse(0, 0, this.r * 2);
+
     // Draw gun
-    rect(0, -3, 30, 6);    
-    
+    rect(0, -3, 30, 6);
+
     pop();
-}
+};
