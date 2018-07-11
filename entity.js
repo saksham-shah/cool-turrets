fc052385de452917c1df447d78004bbc07bba99f
judah-daniels <judahdaniels@outlook.com>
2018-07-11 17:26:44 +0100

    Added momentum

--- entity.js
+++ entity.js
@@ -11,6 +11,8 @@ function Entity(x_, y_, friction_, r_) {
     // R for radius
     this.r = r_;
 
+    this.mass = 10;
+
     this.drawPos = this.pos.copy();
 
     this.collided = false;
@@ -45,21 +47,87 @@ Entity.prototype.futurePos = function() {
     var future = this.pos.copy();
     future.add(p5.Vector.mult(this.vel, dt));
     return future;
-}
+};
 
-Entity.prototype.collide = function(entity) {
-    if (p5.Vector.dist(this.futurePos(), entity.pos) <= this.r + entity.r) {
+Entity.prototype.collide = function(other) {
+    if (p5.Vector.dist(this.futurePos(), other.pos) <= this.r + other.r) {
         this.collided = true;
 
-        //Bounce
-        var normal = p5.Vector.sub(this.pos, entity.pos);
-        normal.normalize();
+        //taken from https://stackoverflow.com/questions/35907053/including-the-law-of-conservation-of-momentum-in-a-simple-collision
 
-        var reflectedVector = reflectVector(this.vel, normal);
-        this.vel = reflectedVector.copy();
-        //this.pos.add(p5.Vector.mult(this.vel, dt));
-        this.acc.add(normal);
-        // console.log(this.vel);
+        //Bounce
+        var bVect = p5.Vector.sub(other.pos, this.pos);
+
+        // precalculate trig values
+        var theta = bVect.heading();
+        var sine = sin(theta);
+        var cosine = cos(theta);
+
+        /* bTemp will hold rotated ball positions. You 
+       just need to worry about bTemp[1] position*/
+        bTemp = [createVector(0, 0), createVector(0, 0)];
+        bTemp[1].x = cosine * bVect.x + sine * bVect.y;
+        bTemp[1].y = cosine * bVect.y - sine * bVect.x;
+
+        vTemp = [createVector(0, 0), createVector(0, 0)];
+
+        vTemp[0].x = cosine * this.vel.x + sine * this.vel.y;
+        vTemp[0].y = cosine * this.vel.y - sine * this.vel.x;
+        vTemp[1].x = cosine * other.vel.x + sine * other.vel.y;
+        vTemp[1].y = cosine * other.vel.y - sine * other.vel.x;
+
+        /* Now that velocities are rotated, you can use 1D
+       conservation of momentum equations to calculate 
+       the final velocity along the x-axis. */
+
+        vFinal = [createVector(0, 0), createVector(0, 0)];
+
+        // final rotated velocity for b[0]
+        vFinal[0].x = ((this.mass - other.mass) * vTemp[0].x + 2 * other.mass * vTemp[1].x) / (this.mass + other.mass);
+        vFinal[0].y = vTemp[0].y;
+
+        // final rotated velocity for b[0]
+        vFinal[1].x = ((other.mass - this.mass) * vTemp[1].x + 2 * this.mass * vTemp[0].x) / (this.mass + other.mass);
+        vFinal[1].y = vTemp[1].y;
+
+        // hack to avoid clumping
+        bTemp[0].x += vFinal[0].x;
+        bTemp[1].x += vFinal[1].x;
+
+        /* Rotate ball positions and velocities back
+               Reverse signs in trig expressions to rotate 
+               in the opposite direction */
+        // rotate balls
+        bFinal = [createVector(0, 0), createVector(0, 0)];
+
+        bFinal[0].x = cosine * bTemp[0].x - sine * bTemp[0].y;
+        bFinal[0].y = cosine * bTemp[0].y + sine * bTemp[0].x;
+        bFinal[1].x = cosine * bTemp[1].x - sine * bTemp[1].y;
+        bFinal[1].y = cosine * bTemp[1].y + sine * bTemp[1].x;
+
+        // update balls to screen position
+        other.pos.x = this.pos.x + bFinal[1].x;
+        other.pos.y = this.pos.y + bFinal[1].y;
+
+        this.pos.add(bFinal[0]);
+
+        // update velocities
+        this.vel.x = cosine * vFinal[0].x - sine * vFinal[0].y;
+        this.vel.y = cosine * vFinal[0].y + sine * vFinal[0].x;
+        other.vel.x = cosine * vFinal[1].x - sine * vFinal[1].y;
+        other.vel.y = cosine * vFinal[1].y + sine * vFinal[1].x;
+
+
+
+
+        /*      var normal = p5.Vector.sub(this.pos, entity.pos);
+              normal.normalize();
+
+              var reflectedVector = reflectVector(this.vel, normal);
+              this.vel = reflectedVector.copy();
+              //this.pos.add(p5.Vector.mult(this.vel, dt));
+              this.acc.add(normal);
+              // console.log(this.vel);*/
     }
 };
 
@@ -102,5 +170,5 @@ Entity.prototype.hitByBullet = function(bullet) {
     }
 
     // console.log(knockback.mag());
-     console.log("Hit for " + bullet.damage + " damage, health left: " + this.health);
+    console.log("Hit for " + bullet.damage + " damage, health left: " + this.health);
 }
